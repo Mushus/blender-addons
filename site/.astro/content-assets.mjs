6e9dfc5288983548c1ab99983910a1d2e1181c60
mushus <6138982+Mushus@users.noreply.github.com>
2026-08-07 18:21:27 +0900

    update gitignore

--- site/.astro/content-assets.mjs
+++
@@ -1,5 +0,0 @@
-
-import __ASTRO_IMAGE_IMPORT_1staQ2 from "../../assets/hero-icon.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx";
-import __ASTRO_IMAGE_IMPORT_2fncNa from "../../../assets/hero-icon.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fen%2Findex.mdx";
-export default new Map([["../../assets/hero-icon.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx", __ASTRO_IMAGE_IMPORT_1staQ2], ["../../../assets/hero-icon.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fen%2Findex.mdx", __ASTRO_IMAGE_IMPORT_2fncNa]]);
-		
